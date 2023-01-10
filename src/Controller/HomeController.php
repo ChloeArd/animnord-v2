5830f763b3d6f42417c1e1dd08a1d451f7a7513c
ChloeArd <chloe.ardoise@gmail.com>
2023-01-10 17:04:22 +0100

    Gestion annonces recentes dans la page d'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdFindRepository;
+use App\Repository\AdLostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(AdLostRepository $adLostRepository, AdFindRepository $adFindRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $adFind = $adFindRepository->findBy([], ['id' => "DESC"], 4);
+        $adLost = $adLostRepository->findBy([], ['id' => "DESC"], 4);
+        return $this->render('home/index.html.twig', [
+            "adFind" => $adFind,
+            "adLost" => $adLost
+        ]);
     }
 
     #[Route('/ad', name: 'ad')]
